a66de5571bc0253518fd86099f276148a613c2ed
yikejason <18215513386@163.com>
2024-04-03 15:08:32 +0800

    httpie 功能完善

--- tool-cli/src/main.rs
+++ tool-cli/src/main.rs
@@ -1,4 +1,9 @@
+use anyhow::{anyhow, Ok, Result};
 use clap::Parser;
+use colored::*;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use std::{collections::HashMap, str::FromStr};
 
 // 定义 HTTPie 的 CLI 的主入口，它包含若干个子命令
 // 下面 /// 的注释是文档，clap 会将其作为 CLI 的帮助
@@ -21,21 +26,135 @@ enum Subcommand {
 #[derive(Parser, Debug)]
 struct Get {
     /// HTTP 请求的url
+    #[arg(value_parser=parse_kv_pair)]
     url: String,
 }
-
 // post 子命令。需要输入一个 URL, 和若干个可选的 key = value, 用于提供 json body
 /// feed post with an url and optional key = value pairs. We will post the data
 /// as Json, and retrieve the response for you
 #[derive(Parser, Debug)]
 struct Post {
     /// HTTP 请求的 URL
+    #[arg(value_parser=parse_url)]
     url: String,
     /// HTTP 请求的 body
-    body: Vec<String>,
+    #[arg(value_parser=parse_kv_pair)]
+    body: Vec<KvPair>,
 }
 
-fn main() {
+/// 命令行中的 key = value 可以通过 parse_kv_pair 解析成 KvPair 结构
+#[derive(Debug, Clone, PartialEq)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+/// 当我们实现 FromStr trait 后，可以用 str.parse() 方法将字符串解析成 KvPair
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用 = 进行 split, 这会得到一个迭代器
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 从迭代器中取第一个结果为 key, 迭代器返回 Some(T)/None
+            // 我们将其转换成 Ok(T)/Err(E), 然后用 ？处理错误
+            k: (split.next().ok_or_else(err)?.to_string()),
+            // 从迭代器中取第二个结果为value
+            v: (split.next().ok_or_else(err)?.to_string()),
+        })
+    }
+}
+
+/// 因为我们为 KvPair 实现了 FromStr, 这里可以直接 s.parse 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    // 这里我们仅仅检查下 URL 是否合法
+    let _url: Url = s.parse()?;
+
+    Ok(s.into())
+}
+
+/// 处理get子命令
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    println!("{:?}", print_resp(resp).await?);
+    Ok(())
+}
+
+/// 处理post子命令
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    println!("{:?}", print_resp(resp).await?);
+    Ok(())
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    print!("\n");
+}
+
+// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        // 其它 mime type, 我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+// 打印整个响应
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+    let mut headers = header::HeaderMap::new();
+    // 为我们的 HTTP 客户端添加一些缺省的 HTTP 头
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    let result = match opts.subcmd {
+        Subcommand::Get(ref args) => get(client, args).await?,
+        Subcommand::Post(ref args) => post(client, args).await?,
+    };
+
+    Ok(result)
 }
